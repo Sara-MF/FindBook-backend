46d5da60a24c5920f4c83c6b0aa3627dd43cc812
Sara-MF <sara.melo.dev@gmail.com>
2024-10-25 18:18:10 -0300

    estruturando o projeto e criando middlewares

---
+++ src/infra/http/express.ts
@@ -0,0 +1,33 @@
+import express, { Application } from "express";
+import cors from "cors";
+import { errorMiddleware } from "../middlewares/errorMiddleware";
+
+class Express {
+
+  app: Application;
+
+  constructor () {
+    this.app = express();
+    this.initMiddlewares();
+    this.errorMiddleware();
+  }
+
+  private initMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors());
+  }
+
+  private errorMiddleware() {
+    this.app.use(errorMiddleware);
+  }
+
+  listen() {
+    this.app.listen(3333, () => {
+      console.log('Server is running on port 3333');
+    })
+  }
+
+}
+
+export default Express
---
+++ src/infra/middlewares/errorMiddleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "../../interfaces/httpException";
+
+export function errorMiddleware(err: HttpException, req: Request, res: Response, next: NextFunction) {
+
+  return res.status(500).json({
+    statusCode: 500,
+    message: err.message || 'Internal Server Error'
+  })
+
+}
